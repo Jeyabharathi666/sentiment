b28e483a9c9b72a4ed38361889fb7c453cff157c
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2025-10-10 16:39:01 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,6 @@
 
 import requests
-from datetime import datetime
+from datetime import datetime,timedelta
 import google_sheets  # Your separate module
 
 SHEET_ID = "1YkTXNX5LAhANICxzu4Tj4Hr6aFMoXdufH_ev1gmRx7c"
@@ -26,11 +26,21 @@ def get_sentiment_for_ticker(ticker):
     
     
     if sentiments:
-        avg_sentiment = sum(sentiments) / len(sentiments)
+        sum_sentiment=0
+        length=0
+        for i in sentiments:
+            if i:
+                sum_sentiment+=i
+                length+=1
+            
+        avg_sentiment = sum_sentiments / length
+        
         if avg_sentiment > 0:
             sentiment_label = "Positive"
         elif avg_sentiment < 0:
             sentiment_label = "Negative"
+        elif (None in sentiments):
+            sentiment_label = "None"
         else:
             sentiment_label = "Neutral"
         return round(avg_sentiment, 4), sentiment_label
@@ -39,22 +49,27 @@ def get_sentiment_for_ticker(ticker):
 
 def main():
     tickers = ["^NSEI", "^NSEBANK"]
-    headers = ["NSE", "SENTIMENT_SCORE", "SENTIMENT", "VALUE"]
+    print_ticker=["NIFTY","NIFTY_BANK"]
+    headers = ["NSE", "SENTIMENT_SCORE", "SENTIMENT",]
     rows = []
 
     # Get sentiment data
     for ticker in tickers:
         sentiment_score, sentiment_label = get_sentiment_for_ticker(ticker)
         if sentiment_score is not None:
-            rows.append([ticker, sentiment_score, sentiment_label, sentiment_score])
+            rows.append([print_ticker, sentiment_score, sentiment_label])
         else:
-            rows.append([ticker, "NO DATA FOUND", "NO DATA FOUND", "NO DATA FOUND"])
+            rows.append([print_ticker, "NO DATA FOUND", "NO DATA FOUND"])
 
     # Append rows to Google Sheet
     google_sheets.update_google_sheet_by_name(SHEET_ID, WORKSHEET_NAME, headers, rows)
 
+    #Convert GMT time to IST
+    gmt_time=datetime.utcnow()
+    ist_time=gmt_time+timedelta(hours=5,minutes=30)
+    
     # Append timestamp as a separate row at the end
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    now = ist_time.strftime("%Y-%m-%d %H:%M:%S")
     google_sheets.append_footer(SHEET_ID, WORKSHEET_NAME, [now])
 
 if __name__ == "__main__":
